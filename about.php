bdabe45c83a21cc569cef8dfc83a3ba59373a1a6
Benhabi Mahdi <k1ghtx@gmail.com>
2016-09-03 01:28:08 +0100

    Create about.php

---
+++ about.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * @Author  : K1ghtx
+ * @Version : 1.0
+ * @Package : Online MD5 Cracker
+ */
+defined ("K1ghtx") or die ("Direct Access Not Allowed !");
+
+?>
+<center>
+	<div class="alert-box alert">Contact Me On Twitter:"@cyb3rp1"</div>
+</center>
+<font color='red'><?php echo $config['sitename']; ?></font> allows you to input an MD5 hash and search for its decrypted state in our database, basically, it's a MD5 cracker / decryption tool.<br><br>
+<b>How many decryptions are in your database ?</b><br><br>
+We have a total of <font color='red'>
+
+<?php
+
+echo mysqli_num_rows (mysqli_query ($mysql_link, "SELECT DISTINCT md5_hash FROM md5;"));
+
+?>
+
+</font>  unique decrypted MD5 hashes.
